ad469624928f0f016b67f410629ba182419af6a8
Ganesh <ganesh.j@softwaysolutions.com>
2019-01-23 15:05:52 +0530

    adds operator learning

---
+++ operator/operators.js
@@ -0,0 +1,58 @@
+/*
+Arithmetic operator
+	Add, subtract, multiply, division, modulus, incremment, decrement
+*/
+
+
+let a = 100;
+let b = 20;
+
+let c = a + b;
+let d = a - b;
+let e = a * b;
+let f = a / b;
+let g = a % b;
+a++
+let h = a;
+b--
+let i = b;
+
+console.log(c + " " + d + " " + e + " " + f + " " + g + " " + h + " " + i);
+
+
+/**
+Comparison operator
+**/
+
+let x = 65;
+
+let y = 75;
+
+console.log(x==y);
+console.log(x!=y);
+console.log(x<=y);
+console.log(x>=y);
+console.log(x<y);
+console.log(x>y);
+
+
+/**
+Comparison operator
+**/
+
+let m = 500;
+let n = 100;
+
+console.log(m += n);
+console.log(m -= n);
+console.log(m *= n);
+console.log(m /= n);
+console.log(m %= n);
+
+/**
+Logical or Boolean Operators: AND, OR, NOT
+**/
+
+console.log (a > b && a > y);
+console.log (a > b || a > y);
+console.log(!(a > b));
